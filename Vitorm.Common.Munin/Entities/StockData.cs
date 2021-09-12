9de881ecb40bd17267bb9a8616d166ae9e721221
Dennis von Bargen <dennis@vonbargen.se>
2021-09-12 13:21:24 +0200

    Refactored StockData

--- Vitorm.Common.Munin/Entities/StockData.cs
+++ Vitorm.Common.Munin/Entities/StockData.cs
@@ -2,32 +2,51 @@
 using System.Diagnostics;
 using DvBCrud.EFCore.Entities;
 
-namespace Vitorm.Common.Core
+namespace Vitorm.Common.Munin.Entities
 {
     public class StockData : BaseEntity<int>
     {
         #region Prices
         
         public string Name { get; set; }
-        public string Index { get; set; }
-        public DateTimeOffset Timestamp { get; set; }
-        public decimal LatestPrice { get; set; }
-        public decimal DeltaPrice { get; set; }
-        public double DeltaPricePercent { get; set; }
-        public decimal HighestPrice { get; set; }
-        public decimal LowestPrice { get; set; }
-        public ulong Volume { get; set; }
-        public decimal MarketValue { get; set; }
         
+        public string Index { get; set; }
+        
+        public DateTimeOffset Timestamp { get; set; }
+        
+        /// <summary>
+        /// Three-letter currency code as defined in ISO 4217
+        /// </summary>
+        public string CurrencyCode { get; set; }
+        
+        public decimal LatestPrice { get; set; }
+        
+        public decimal DeltaPrice { get; set; }
+        
+        public double DeltaPricePercent { get; set; }
+        
+        public decimal HighestPrice { get; set; }
+        
+        public decimal LowestPrice { get; set; }
+        
+        public ulong Volume { get; set; }
+        
+        public decimal MarketValue { get; set; }
+
         #endregion
         
         #region Key Performance Indicators
 
-        public int PeNumber { get; set; }
-        public double PsNumber { get; set; }
-        public double ProfitPerStock { get; set; }
-        public double AdjustedEquityPerStock { get; set; }
-        public decimal DividendPerStock { get; set; }
+        public double PriceToEarningsRatio { get; set; }
+        
+        public double PriceToSalesRatio { get; set; }
+        
+        public double EarningsPerShare { get; set; }
+        
+        public double AdjustedEquityPerShare { get; set; }
+        
+        public decimal DividendYield { get; set; }
+        
         public double DirectDividendPercent { get; set; }
         
         #endregion
@@ -45,6 +64,7 @@ namespace Vitorm.Common.Core
             Name = data.Name;
             Index = data.Index;
             Timestamp = data.Timestamp;
+            CurrencyCode = data.CurrencyCode;
             LatestPrice = data.LatestPrice;
             DeltaPrice = data.DeltaPrice;
             DeltaPricePercent = data.DeltaPricePercent;
@@ -53,11 +73,11 @@ namespace Vitorm.Common.Core
             Volume = data.Volume;
             MarketValue = data.MarketValue;
 
-            PeNumber = data.PeNumber;
-            PsNumber = data.PsNumber;
-            ProfitPerStock = data.ProfitPerStock;
-            AdjustedEquityPerStock = data.AdjustedEquityPerStock;
-            DividendPerStock = data.DividendPerStock;
+            PriceToEarningsRatio = data.PriceToEarningsRatio;
+            PriceToSalesRatio = data.PriceToSalesRatio;
+            EarningsPerShare = data.EarningsPerShare;
+            AdjustedEquityPerShare = data.AdjustedEquityPerShare;
+            DividendYield = data.DividendYield;
             DirectDividendPercent = data.DirectDividendPercent;
         }
     }
